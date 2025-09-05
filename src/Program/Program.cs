7c245b2f442311a3e0a7ed63cb8db5242eb7bf6b
Marcoss141 <marcoscc2006@gmail.com>
2025-09-05 15:21:13 -0300

    rama Marcos

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -6,7 +6,34 @@ namespace Ucu.Poo.GameOfLife
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool[,] b //variable que representa el tablero
+            int width //variabe que representa el ancho del tablero
+            int height //variabe que representa altura del tablero
+            while (true)
+            {
+                Console.Clear();
+                StringBuilder s = new StringBuilder();
+                for (int y = 0; y<height;y++)
+                {
+                    for (int x = 0; x<width; x++)
+                    {
+                        if(b[x,y])
+                        {
+                            s.Append("|X|");
+                        }
+                        else
+                        {
+                            s.Append("___");
+                        }
+                    }
+                    s.Append("\n");
+                }
+                Console.WriteLine(s.ToString());
+                //=================================================
+                //Invocar método para calcular siguiente generación
+                //=================================================
+                Thread.Sleep(300);
+            }
         }
     }
 }
