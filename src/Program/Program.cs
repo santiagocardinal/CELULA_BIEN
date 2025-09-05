13e9741373a9b9bb6df8f4674e31e73be5e698de
Marcoss141 <marcoscc2006@gmail.com>
2025-09-05 15:35:16 -0300

    rama

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -7,6 +7,7 @@ namespace Ucu.Poo.GameOfLife
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola");
         }
     }
 }
