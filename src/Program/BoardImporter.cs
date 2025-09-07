ffe9b542ff932ce0555c6c8ee524b4a31936631f
Lucia Rodriguez Penades <lucia.rodriguezp@correo.ucu.edu.uy>
2025-09-07 13:21:09 -0300

    se corrige de manera momentanea el acceso al archivo de las celulas

--- src/Program/BoardImporter.cs
+++ src/Program/BoardImporter.cs
@@ -5,6 +5,35 @@ namespace Ucu.Poo.GameOfLife
 {
     public class BoardImporter
     {
+        //VER TEMA RUTA RELATIVA Y/O HIBRIDA
+        static string url = @"C:\Users\Lucia\programacion2\jueguito\assets\board.txt";
+        static string content = File.ReadAllText(url);
+        static string[] contentLines = content.Split('\n').Select(l => l.Trim('\r')).ToArray();
+        private Cell[,] board;
+
+        public BoardImporter()
+        {
+            board = new Cell[contentLines.Length, contentLines[0].Length];
+        }
         
+        public Cell [,] Import()
+        {
+            
+            for (int  y=0; y<contentLines.Length;y++)
+            {
+                for (int x=0; x<contentLines[y].Length; x++)
+                {
+                    if(contentLines[y][x]=='1')
+                    {
+                        board[x,y]=new Cell(true);
+                    }
+                    else
+                    {
+                        board[x,y]=new Cell(false);
+                    }
+                }
+            }
+            return board;
+        }
     }
 }
